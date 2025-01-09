33190456754708cd843c21a0392e20ce90e9e320
Sanjeev <sanjeevkrcs@gmail.com>
2025-01-09 23:22:58 +0530

    config changes

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 # config.py
-ATTACKBOX_SERVER_DOMAIN = 'localhost:8000'
+ATTACKBOX_SERVER_DOMAIN = 'backend.attackbox.online'
 PROTOCOL = 'http'
+KAFKA_URL = '172.210.111.156:9092'
+
+
 AUTH_URL_TEMPLATE = '{protocol}://{tenant}.{domain}/api/v1/gateway/authenticate/'
 SSE_URL_TEMPLATE = '{protocol}://{tenant}.{domain}/api/v1/gateway/events/{gateway_id}/'
 HEARTBEAT_URL_TEMPLATE = '{protocol}://{tenant}.{domain}/api/v1/gateway/{gateway_id}/heartbeat'
-KAFKA_URL = 'localhost:9092'
